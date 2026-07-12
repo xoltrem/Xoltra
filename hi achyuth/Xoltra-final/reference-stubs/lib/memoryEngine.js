495e519dcedd51595d925b9ba629993250a9ff96
fellowlad <jishanth.premanand@gmail.com>
2026-07-12 10:25:24 +0530

    Delete hi achyuth/Xoltra-final directory

--- hi achyuth/Xoltra-final/reference-stubs/lib/memoryEngine.js
+++
@@ -1,6 +0,0 @@
-import { db } from './db';
-
-export async function getMemoryForUser(userId, query) {
-  const rows = await db.memory.findRelevant(userId, query, { topK: 5 });
-  return rows;
-}
--- hi achyuth/Xoltra-final/reference-stubs/pages/api/memory/index.js
+++
@@ -1,8 +0,0 @@
-import { withAuth } from '../../../lib/withAuth';
-import { getMemoryForUser } from '../../../lib/memoryEngine';
-
-export default withAuth(async function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).end();
-  const memory = await getMemoryForUser(req.userId, req.query.query || '');
-  res.status(200).json({ memory });
-});
