bf145448f7f5714836a704e79add667b2523c10c
xoltrem <xoltrem@gmail.com>
2026-08-03 15:37:13 +0530

    Update schema path in drizzle configuration

--- frontend/drizzle.config.ts
+++ frontend/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/lib/schema.ts",
+  schema: "./app/lib/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
